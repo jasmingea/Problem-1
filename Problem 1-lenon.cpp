84626f75f8fa9a4d72cfe609bd8e4b868a93a4f8
Jasmin Lenon <47447983+jasmingea@users.noreply.github.com>
2019-03-08 19:16:16 +0800

    Add files via upload

---
+++ Problem 1-lenon.cpp
@@ -0,0 +1,134 @@
+// This program shows the Total Ticket sales.
+
+#include <iostream>
+#include <conio.h>
+#include <string>
+
+using namespace std;
+
+int main ()
+{
+	int ticketPrice;
+	int numberOfTicketsSold;
+	double totalSales;
+
+	cout << "Game1" << endl;
+	cout << "ticketPrice" << endl;
+	cout << "$250" << endl;
+	cout << "Enter number Of Tickets Sold" << endl;
+	cin >> numberOfTicketsSold;
+	cout << "Total Sales" << endl;
+	cout << "250 * 5750 = " << 250 * 5750 << endl;
+
+	cout << "ticketPrice" << endl;
+	cout << "$100" << endl;
+	cout << "Enter number Of Tickets Sold" << endl;
+	cin >> numberOfTicketsSold;
+	cout << "Total Sales" << endl;
+	cout << "100 * 28000 = " << 100 * 28000 << endl;
+
+	cout << "ticketPrice" << endl;
+	cout << "$50" << endl;
+	cout << "Enter number Of Tickets Sold" << endl;
+	cin >> numberOfTicketsSold;
+	cout << "Total Sales" << endl;
+	cout << "50 * 35750 = " << 50 * 35750 << endl;
+
+	cout << "ticketPrice" << endl;
+	cout << "$25" << endl;
+	cout << "Enter number Of Tickets Sold" << endl;
+	cin >> numberOfTicketsSold;
+	cout << "Total Sales" << endl;
+	cout << "25 * 18750 = " << 25 * 18750 << endl;
+
+	cout << "Game2" << endl;
+	cout << "ticketPrice" << endl;
+	cout << "$250" << endl;
+	cout << "Enter number Of Tickets Sold" << endl;
+	cin >> numberOfTicketsSold;
+	cout << "Total Sales" << endl;
+	cout << "250 * 5750 = " << 250 * 5750 << endl;
+
+	cout << "ticketPrice" << endl;
+	cout << "$100" << endl;
+	cout << "Enter number Of Tickets Sold" << endl;
+	cin >> numberOfTicketsSold;
+	cout << "Total Sales" << endl;
+	cout << "100 * 28000 = " << 100 * 28000 << endl;
+
+	cout << "ticketPrice" << endl;
+	cout << "$50" << endl;
+	cout << "Enter number Of Tickets Sold" << endl;
+	cin >> numberOfTicketsSold;
+	cout << "Total Sales" << endl;
+	cout << "50 * 35750 = " << 50 * 35750 << endl;
+
+	cout << "ticketPrice" << endl;
+	cout << "$25" << endl;
+	cout << "Enter number Of Tickets Sold" << endl;
+	cin >> numberOfTicketsSold;
+	cout << "Total Sales" << endl;
+	cout << "25 * 18750 = " << 25 * 18750 << endl;
+
+	cout << "Game3" << endl;
+	cout << "ticketPrice" << endl;
+	cout << "$250" << endl;
+	cout << "Enter number Of Tickets Sold" << endl;
+	cin >> numberOfTicketsSold;
+	cout << "Total Sales" << endl;
+	cout << "250 * 5750 = " << 250 * 5750 << endl;
+
+	cout << "ticketPrice" << endl;
+	cout << "$100" << endl;
+	cout << "Enter number Of Tickets Sold" << endl;
+	cin >> numberOfTicketsSold;
+	cout << "Total Sales" << endl;
+	cout << "100 * 28000 = " << 100 * 28000 << endl;
+
+	cout << "ticketPrice" << endl;
+	cout << "$50" << endl;
+	cout << "Enter number Of Tickets Sold" << endl;
+	cin >> numberOfTicketsSold;
+	cout << "Total Sales" << endl;
+	cout << "50 * 35750 = " << 50 * 35750 << endl;
+
+	cout << "ticketPrice" << endl;
+	cout << "$25" << endl;
+	cout << "Enter number Of Tickets Sold" << endl;
+	cin >> numberOfTicketsSold;
+	cout << "Total Sales" << endl;
+	cout << "25 * 18750 = " << 25 * 18750 << endl;
+
+	cout << "Game4" << endl;
+	cout << "ticketPrice" << endl;
+	cout << "$250" << endl;
+	cout << "Enter number Of Tickets Sold" << endl;
+	cin >> numberOfTicketsSold;
+	cout << "Total Sales" << endl;
+	cout << "250 * 5750 = " << 250 * 5750 << endl;
+
+	cout << "ticketPrice" << endl;
+	cout << "$100" << endl;
+	cout << "Enter number Of Tickets Sold" << endl;
+	cin >> numberOfTicketsSold;
+	cout << "Total Sales" << endl;
+	cout << "100 * 28000 = " << 100 * 28000 << endl;
+
+	cout << "ticketPrice" << endl;
+	cout << "$50" << endl;
+	cout << "Enter number Of Tickets Sold" << endl;
+	cin >> numberOfTicketsSold;
+	cout << "Total Sales" << endl;
+	cout << "50 * 35750 = " << 50 * 35750 << endl;
+
+	cout << "ticketPrice" << endl;
+	cout << "$25" << endl;
+	cout << "Enter number Of Tickets Sold" << endl;
+	cin >> numberOfTicketsSold;
+	cout << "Total Sales" << endl;
+	cout << "25 * 18750 = " << 25 * 18750 << endl;
+	_getch();
+
+	return 0;
+
+}
